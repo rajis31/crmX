728fbf789f44b52e245baebdb6a8311881807f21
Raji <raj350@mail.fresnostate.edu>
2022-03-01 16:58:16 -0800

    Update Topbar.jsx

--- frontend/src/components/Topbar/Topbar.jsx
+++ frontend/src/components/Topbar/Topbar.jsx
@@ -9,7 +9,7 @@ export default function Topbar() {
     <div className='topbar'>
         <div className='topbarWrapper'>
             <div className="topLeft">
-                <span className="logo">Admin</span>
+                <span className="logo">CRM</span>
             </div>
             <div className="topRight">
                 <div className="topbarIconContainer">
@@ -29,3 +29,4 @@ export default function Topbar() {
     </div>
   )
 }
+
