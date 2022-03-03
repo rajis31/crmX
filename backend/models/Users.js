b76d5c0b6188f8cf4bd766d5269f026d8fb69e32
Raji <raj350@mail.fresnostate.edu>
2022-03-03 13:34:15 -0800

    Update Users.js

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -1,4 +1,5 @@
 const db = require("../config/db");
+const bcrypt = require("bcrypt");
 
 class Users{
 
@@ -16,19 +17,17 @@ class Users{
             );
         `;
 
-        console.log(sql);
-
         return db.execute(sql);
     }
     
 
-    insert_data(username, password, date_created, email){
+    async insert_data(username, password, date_created, email){
         /**
          * Insert a new row into table
          */
 
         this.username     = username;
-        this.password     = password; 
+        this.password     = await bcrypt.hash(password,10); 
         this.email        = email; 
         this.date_created = date_created;
 
