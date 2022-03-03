3efc5cb810f072e0a6ddc61fd5478ce5200a5186
Raji <raj350@mail.fresnostate.edu>
2022-03-03 11:06:39 -0800

    Create Notes.js

---
+++ backend/models/Notes.js
@@ -0,0 +1,64 @@
+const db = require("../config/db");
+
+class Notes{
+
+    constructor(username, title, body, date_created){
+        this.username     = username;
+        this.title        = title; 
+        this.body         = body; 
+        this.date_created = date_created;
+        this.tablename    = "notes";
+    }
+
+    static create_table(){
+        let sql = `
+            create table if not exists ${this.tablename} (
+                id int primary key auto_increment, 
+                username varchar(255),
+                title varchar(255),
+                body text, 
+                date_created date
+            );
+        `;
+
+        return db.execute(sql);
+    }
+    
+
+    insert_data(){
+        /**
+         * Insert a new row into tab;e
+         */
+
+        let sql = `
+            INSERT INTO notes(username, title, body, date_created) VALUES 
+            ('${this.username}','${this.title}','${this.body}', '${this.date_created}');
+        `;
+
+        return db.execute(sql);
+    }
+
+    findAll(){
+        /**
+         * Retrieve all rows
+         */
+
+        let sql =  `
+            SELECT * FROM ${this.tablename} where username = '${this.username}'; 
+        `
+        return db.execute(sql);
+    }
+
+    findById(id){
+        /**
+         * Retrieve certain row
+         */
+
+        let sql =  `
+            SELECT * FROM ${this.tablename} where username = '${this.username}' and id=${id}; 
+        `
+        return db.execute(sql);
+    }
+}
+
+module.exports = Notes;
