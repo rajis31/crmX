0c8f21396c41008b835e323d0544e38d8d67f2ab
Raji <raj350@mail.fresnostate.edu>
2022-03-03 16:58:54 -0800

    Update DataTable.jsx

--- frontend/src/components/DataTable/DataTable.jsx
+++ frontend/src/components/DataTable/DataTable.jsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
@@ -10,171 +11,32 @@ import TableContainer from '@mui/material/TableContainer';
 import TableFooter from '@mui/material/TableFooter';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import IconButton from '@mui/material/IconButton';
-import FirstPageIcon from '@mui/icons-material/FirstPage';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import LastPageIcon from '@mui/icons-material/LastPage';
 import axios from "axios";
 
-function TablePaginationActions(props) {
+function DataTable(props) {
   const theme = useTheme();
   const { count, page, rowsPerPage, onPageChange } = props;
+  const [rows, setRows] = useState([]);
 
-  axios.get('http://localhost:3000/notes/')
-  .then((response) => {
-    console.log(response);
-  }, (error) => {
-    console.log(error);
-  });
+  useEffect(() => {
+    const fetchData = async () =>{
+      try {
+        const response = await axios.get('http://localhost:3000/notes/')
+        setRows(response);
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
 
+    fetchData();
+  }, []);
 
-  const handleFirstPageButtonClick = (event) => {
-    onPageChange(event, 0);
-  };
-
-  const handleBackButtonClick = (event) => {
-    onPageChange(event, page - 1);
-  };
-
-  const handleNextButtonClick = (event) => {
-    onPageChange(event, page + 1);
-  };
-
-  const handleLastPageButtonClick = (event) => {
-    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-  };
-
-  return (
-    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
-      <IconButton
-        onClick={handleFirstPageButtonClick}
-        disabled={page === 0}
-        aria-label="first page"
-      >
-        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-      </IconButton>
-      <IconButton
-        onClick={handleBackButtonClick}
-        disabled={page === 0}
-        aria-label="previous page"
-      >
-        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-      </IconButton>
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="next page"
-      >
-        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-      </IconButton>
-      <IconButton
-        onClick={handleLastPageButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="last page"
-      >
-        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-      </IconButton>
-    </Box>
-  );
+  
+    return (
+      <>
+      </>
+  )
+  
 }
 
-TablePaginationActions.propTypes = {
-  count: PropTypes.number.isRequired,
-  onPageChange: PropTypes.func.isRequired,
-  page: PropTypes.number.isRequired,
-  rowsPerPage: PropTypes.number.isRequired,
-};
-
-function createData(id,title, date, body) {
-  return {id, title, date, body };
-}
-
-const rows = [
-  createData(1,'Test 1', "02-21-2022", "This is test 1"),
-  createData(2,'Test 2', "02-18-2022", "This is test 2"),
-  createData(3,'Test 3', "01-13-2022", "This is test 3"),
-  createData(4,'Test 4', "01-21-2022", "This is test 4"),
-  createData(5,'Test 5', "02-05-2022", "This is test 5"),
-  createData(6,'Test 6', "01-04-2022", "This is test 6"),
-  createData(7,'Test 7', "01-22-2022", "This is test 7"),
-  createData(8,'Test 8', "01-15-2022", "This is test 8"),
-  createData(9,'Test 9', "01-21-2021", "This is test 9"),
-];
-
-export default function CustomPaginationActionsTable() {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-  // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  return (
-    <TableContainer component={Paper} sx={{ miWidth: 400, maxWidth: 700 }}>
-      <Table  aria-label="custom pagination table">
-      <TableHead>
-          <TableRow>
-            <TableCell style={{fontWeight:"bold"}} align="right" >Title</TableCell>
-            <TableCell style={{fontWeight:"bold"}} align="right">Date</TableCell>
-            <TableCell style={{fontWeight:"bold"}} align="right">Summary</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {(rowsPerPage > 0
-            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            : rows
-          ).map((row) => (
-            <TableRow key={row.id}>
-              <TableCell component="th" scope="row" style={{ width: 160 }}>
-                {row.title}
-              </TableCell>
-              <TableCell style={{ width: 160 }} align="right">
-                {row.date}
-              </TableCell>
-              <TableCell style={{ width: 160 }} align="right">
-                {row.body}
-              </TableCell>
-            </TableRow>
-          ))}
-
-          {emptyRows > 0 && (
-            <TableRow style={{ height: 53 * emptyRows }}>
-              <TableCell colSpan={6} />
-            </TableRow>
-          )}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-              colSpan={3}
-              count={rows.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              SelectProps={{
-                inputProps: {
-                  'aria-label': 'rows per page',
-                },
-                native: true,
-              }}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-              ActionsComponent={TablePaginationActions}
-            />
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </TableContainer>
-  );
-}
+export default DataTable;
