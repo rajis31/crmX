b813ccd21561c64a36666e174844d54d9d8ceb69
Raji <raj350@mail.fresnostate.edu>
2022-03-07 16:04:23 -0800

    Update Login.jsx

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,30 +1,71 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Login.css";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GoogleIcon from '@mui/icons-material/Google';
+import { Button, TextField } from '@mui/material';
+import { useNavigate as navigate } from 'react-router-dom';
+import axios from "axios";
 
 export default function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    function handleLoginBtn(e) {
+
+    }
+
+    function handleRegisterBtn(e) {
+        navigate("register");
+    }
+
+    function handleForgotBtn(e) {
+        navigate("forgot");
+    }
     return (
-        <div className='container'>
+        <div className='login-form-container'>
             <h1>login</h1>
             <form>
                 <label for="username">username</label>
-                <input type="text" name="username" placeholder="Type Your Username"/>
+                <TextField 
+                        placeholder='Type your Username' 
+                        type="text" 
+                        name='username' 
+                        onChange={(e) => { setUsername(e.target.value) }} 
+                />
                 <label for="password">password</label>
-                <input type="text" name="password" placeholder="Type Your Password"/>
+                <TextField 
+                        placeholder='Type your Password' 
+                        type="password" 
+                        name='password'
+                        onChange={(e) => { setPassword(e.target.value) }}  
+                />
             </form>
             <p class="forgot">forgot password?</p>
-            <a href="#" className="btn"><button>login</button></a>
-            <p className="signup">or sign up using</p>
-            
-            <div className="social">
-                <i className="fab fa-facebook-f"></i>
-                <i className="fab fa-twitter"></i>
-                <i className="fab fa-google"></i>
+            <div className='login-form__btn-group'>
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={handleLoginBtn}
+                >
+                    Login Here
+                </Button>
+
+                <Button
+                    variant="contained"
+                    color="success"
+                    onClick={handleRegisterBtn}
+                >
+                    Register Here
+                </Button>
             </div>
-                
-            <div className="last-content">
-                <p>or sign up using</p>
-                <p className="uppercase">signup</p>
-            </div>        
+
+            <div className="social">
+                <FacebookIcon className='social-icon' />
+                <TwitterIcon className='social-icon' />
+                <GoogleIcon className='social-icon' />
+            </div>
         </div>
 
     )
