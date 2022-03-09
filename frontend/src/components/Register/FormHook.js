5d1012df5b803b2f087b5850fe4ef798d10f7bdf
Raji <raj350@mail.fresnostate.edu>
2022-03-09 12:18:19 -0800

    Create FormHook.js

---
+++ frontend/src/components/Register/FormHook.js
@@ -0,0 +1,24 @@
+const validate = (inputs) =>{
+    const errors = {};
+    console.log(inputs);
+
+    if(inputs.email.length === 0){
+        errors.emailError = true;
+    }
+
+    if(inputs.username.length === 0){
+        errors.usernameError = true;
+    }
+
+    if(inputs.password.length === 0){
+        errors.passwordError = true;
+    }
+
+    if(inputs.username !== inputs.confirm_password){
+        errors.confirmpasswordError = true;
+    }
+
+    return errors;
+}
+
+export default validate;
