202cafa94572e438a523498b1d5f35d3449eee76
Raji <raj350@mail.fresnostate.edu>
2022-03-10 12:58:30 -0800

    Update Metricbar.jsx

--- frontend/src/components/Metricbar/Metricbar.jsx
+++ frontend/src/components/Metricbar/Metricbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Metricbar.css"
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-
+import ArrowUpwardIcon  from '@mui/icons-material/ArrowUpward'
 
 function Metricbar() {
     return (
@@ -12,6 +12,26 @@ function Metricbar() {
                     <span className="metric-bar__value">240</span>
                     <span className="metric-bar__value-change">
                         -11.4 <ArrowDownwardIcon className="metric-bar__icon negative" />
+                    </span>
+
+                </div>
+            </div>
+            <div className="metric">
+                <div className="metric-bar__title"># of Contacts</div>
+                <div className='metric-bar__container'>
+                    <span className="metric-bar__value num_of_contacts">20</span>
+                    <span className="metric-bar__value-change">
+                        +1 <ArrowUpwardIcon className="metric-bar__icon positive" />
+                    </span>
+
+                </div>
+            </div>
+            <div className="metric">
+                <div className="metric-bar__title">Avg Clients Added (per day)</div>
+                <div className='metric-bar__container'>
+                    <span className="metric-bar__value avg_add_customers">2.55</span>
+                    <span className="metric-bar__value-change">
+                        -2 <ArrowDownwardIcon className="metric-bar__icon negative" />
                     </span>
 
                 </div>
