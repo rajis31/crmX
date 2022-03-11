7e13f4888ed314d0a1a268cbc5e9dda99d7bd195
Raji <raj350@mail.fresnostate.edu>
2022-03-10 16:58:03 -0800

    Create sampleData.jsx

---
+++ frontend/src/data/sampleData.jsx
@@ -0,0 +1,19 @@
+
+
+const sampleData = ()=>{
+    const data = [
+            {
+                "name": "raji",
+                "profit": 2500
+            },
+            {
+                "name": "raji",
+                "profit": 2500
+            }
+    ];
+
+    return data;
+
+}
+
+export default sampleData;
