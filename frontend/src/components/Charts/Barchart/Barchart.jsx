45204213f4cc4bd1868cf60ec515b2b33c5295c7
Raji <raj350@mail.fresnostate.edu>
2022-03-11 11:34:48 -0800

    Update Barchart.jsx

--- frontend/src/components/Charts/Barchart/Barchart.jsx
+++ frontend/src/components/Charts/Barchart/Barchart.jsx
@@ -11,33 +11,59 @@ import {
     ResponsiveContainer
 } from 'recharts';
 
-const Barchart = ({ xAxisDataKey,barDataKey, fill, data }) => {
-    console.log("XDataKey: "+xAxisDataKey);
-    console.log("barDataKey: "+barDataKey);
-    console.log("fill: "+fill);
-    console.log(data);
-    
+const Barchart = ({ xAxisDataKey, barDataKey, fill, data, title }) => {
+
     return (
-        <ResponsiveContainer width="100%" height={300} className="barchart" >
-            <BarChart
-                width={500}
-                height={300}
-                data={data}
-                margin={{
-                    top: 5,
-                    right: 30,
-                    left: 20,
-                    bottom: 5,
-                }}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey={xAxisDataKey} />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey={barDataKey} fill={fill} />
-            </BarChart>
-        </ResponsiveContainer>
+        <>
+            { data?.length > 0 ?
+                <>
+                    <div className='chart-title'> {title} </div>
+                    <ResponsiveContainer width="100%" height={300} className="barchart" >
+                        <BarChart
+                            width={500}
+                            height={300}
+                            data={data}
+                            margin={{
+                                top: 5,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey={xAxisDataKey} />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Bar dataKey={barDataKey} fill={fill} />
+                        </BarChart>
+                    </ResponsiveContainer>
+                </> :   <>
+                    <div className='chart-title'> {title} </div>
+                    <ResponsiveContainer width="100%" height={300} className="barchart" >
+                        <BarChart
+                            width={500}
+                            height={300}
+                            data={[{name:"No Data", y: 0  }]}
+                            margin={{
+                                top: 5,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Bar dataKey="y" fill={fill} />
+                        </BarChart>
+                    </ResponsiveContainer>
+                </> 
+
+            }
+        </>
     )
 };
 
