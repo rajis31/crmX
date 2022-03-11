a15a13a1d1bb4722efbe392ee1ac315580696e0c
Raji <raj350@mail.fresnostate.edu>
2022-03-11 11:34:22 -0800

    Update Dashboard.jsx

--- frontend/src/Pages/Dashboard/Dashboard.jsx
+++ frontend/src/Pages/Dashboard/Dashboard.jsx
@@ -4,17 +4,32 @@ import Topbar from '../../components/Topbar/Topbar';
 import Metricbar from '../../components/Metricbar/Metricbar';
 import Barchart from '../../components/Charts/Barchart/Barchart';
 import sampleData from '../../data/sampleData';
+import axios from "axios";
 
 function Dashboard() {
-  const data = sampleData();
+
+
+  let data = sampleData();
+  
+  data = data?.map(e => {
+    e.name = e.name.toUpperCase()
+    return e;
+  });
+
   return (
     <>
-       <Topbar />
-       <Sidebar />
-       <div>
-         <Metricbar />
-         <Barchart xAxisDataKey="name" barDataKey="profit" fill="#000" data={data} />
-       </div>
+      <Topbar />
+      <Sidebar />
+      <div>
+        <Metricbar />
+        <Barchart
+          xAxisDataKey="name"
+          barDataKey="profit"
+          fill="#4361ee"
+          data={data}
+          title={"Top 5 Customers"}
+        />
+      </div>
     </>
 
   )
