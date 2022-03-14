942cd9b024dfd3cab074e9f87aa09f7472484d58
Raji <raj350@mail.fresnostate.edu>
2022-03-14 16:31:23 -0700

    Update Table.jsx

--- frontend/src/components/Table/Table.jsx
+++ frontend/src/components/Table/Table.jsx
@@ -1,39 +1,63 @@
 import React, { useState } from "react";
-
 import useTable from "./Hook";
 import styles from "./Table.css";
 import TableFooter from "./TableFooter";
+import { Typography } from '@mui/material';
 
-const Table = ({ data, rowsPerPage }) => {
-  const [page, setPage] = useState(1);
-  const { slice, range } = useTable(data, page, rowsPerPage);
+
+const Table = ({ data, rowsPerPage, columns }) => {
+  const pageRange = (data, rowsPerPage) => {
+      const range    = [];
+      const numPages = Math.ceil(data.length / rowsPerPage);
+    
+      for(let i = 1; i<=numPages; i++){
+        range.push(i);
+      }
+
+      return range;
+  }
+
+  let range = pageRange(data,rowsPerPage);
+  console.log(range);
+
+
 
 
   return (
+    data?.length > 0 ?
     <>
       <table className={styles.table}>
         <thead className={styles.tableRowHeader}>
           <tr>
-            
-            <th className={styles.tableHeader}>Country</th>
-            <th className={styles.tableHeader}>Capital</th>
-            <th className={styles.tableHeader}>Language</th>
+            {
+              columns.map((col, idx) => (
+                 <th className={styles.tableHeader} key={idx} >{col}</th>
+              ))
+            }
           </tr>
         </thead>
         <tbody>
+              {
+                data.map((datum,idx2) => {
+                    let values = Object.values(datum);
 
-          {slice.map((el) => (
-            <tr className={styles.tableRowItems} key={el.id}>
-              <td className={styles.tableCell}>{el.name}</td>
-              <td className={styles.tableCell}>{el.capital}</td>
-              <td className={styles.tableCell}>{el.language}</td>
-            </tr>
-          ))}
-
+                    return (
+                      <tr key={idx2}>
+                          { values.map((val,idx) => ( <td key={idx}>{val}</td>  )) }
+                      </tr>
+                    )
+                })
+              }
         </tbody>
       </table>
-      <TableFooter range={range} slice={slice} setPage={setPage} page={page} />
+      {/* <TableFooter range={range} slice={slice} setPage={setPage} page={page} /> */}
+    </> : 
+    <>
+     <Typography variant="h4" >
+        No Data Available
+    </Typography>
     </>
+
   );
 };
 
