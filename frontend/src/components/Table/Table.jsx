12e829f5452cd09b5a93fc46fbc7839c2f160127
Raj <raj350@mail.fresnostate.edu>
2022-03-15 07:06:39 -0700

    working on table component

--- frontend/src/components/Table/Table.jsx
+++ frontend/src/components/Table/Table.jsx
@@ -1,62 +1,70 @@
-import React, { useState } from "react";
-import useTable from "./Hook";
+import React, { useState, useEffect } from "react";
 import styles from "./Table.css";
 import TableFooter from "./TableFooter";
 import { Typography } from '@mui/material';
 
 
 const Table = ({ data, rowsPerPage, columns }) => {
-  const pageRange = (data, rowsPerPage) => {
-      const range    = [];
-      const numPages = Math.ceil(data.length / rowsPerPage);
-    
-      for(let i = 1; i<=numPages; i++){
-        range.push(i);
-      }
 
-      return range;
+  const [page, setPage] = useState(1);
+  const [tableRange, setTableRange] = useState([]);
+  const [slice, setSlice] = useState([]);
+
+  const pageRange = (data, rowsPerPage) => {
+    const range = [];
+    const numPages = Math.ceil(data.length / rowsPerPage);
+
+    for (let i = 1; i <= numPages; i++) {
+      range.push(i);
+    }
+
+    return range;
   }
 
-  let range = pageRange(data,rowsPerPage);
-  console.log(range);
+  const slideData = (data, rowsPerPage, page) => {
+    return data.slice((page - 1) * rowsPerPage, page * rowsPerPage);
+  }
 
+  useEffect(() => {
+    setSlice(slideData(data, rowsPerPage, page));
+  }, [page, data]);
 
 
 
   return (
-    data?.length > 0 ?
-    <>
-      <table className={styles.table}>
-        <thead className={styles.tableRowHeader}>
-          <tr>
-            {
-              columns.map((col, idx) => (
-                 <th className={styles.tableHeader} key={idx} >{col}</th>
-              ))
-            }
-          </tr>
-        </thead>
-        <tbody>
+    slice?.length > 0 ?
+      <>
+        <table className={styles.table}>
+          <thead className={styles.tableRowHeader}>
+            <tr>
               {
-                data.map((datum,idx2) => {
-                    let values = Object.values(datum);
-
-                    return (
-                      <tr key={idx2}>
-                          { values.map((val,idx) => ( <td key={idx}>{val}</td>  )) }
-                      </tr>
-                    )
-                })
+                columns.map((col, idx) => (
+                  <th className={styles.tableHeader} key={idx} >{col}</th>
+                ))
               }
-        </tbody>
-      </table>
-      {/* <TableFooter range={range} slice={slice} setPage={setPage} page={page} /> */}
-    </> : 
-    <>
-     <Typography variant="h4" >
-        No Data Available
-    </Typography>
-    </>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              data.map((datum, idx2) => {
+                let values = Object.values(datum);
+
+                return (
+                  <tr key={idx2}>
+                    {values.map((val, idx) => (<td key={idx}>{val}</td>))}
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+        {/* <TableFooter range={range} slice={slice} setPage={setPage} page={page} /> */}
+      </> :
+      <>
+        <Typography variant="h4" >
+          No Data Available
+        </Typography>
+      </>
 
   );
 };
