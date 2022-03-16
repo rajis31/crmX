bab654959530c26badd9747888428d77a6998efc
Raji <raj350@mail.fresnostate.edu>
2022-03-16 16:27:52 -0700

    Update Metricbar.jsx

--- frontend/src/components/Metricbar/Metricbar.jsx
+++ frontend/src/components/Metricbar/Metricbar.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Metricbar.css"
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-import ArrowUpwardIcon  from '@mui/icons-material/ArrowUpward'
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import axios from 'axios';
 
 function Metricbar() {
+    const [metrics, setMetrics] = useState([]);
+
+    useEffect(async () => {
+        let result = await axios.get("http://localhost:3000/stats/get_metric_stats");
+        setMetrics(result?.data);
+    }, []);
+
+
+
     return (
         <div className='metric-bar'>
             <div className="metric">
                 <div className="metric-bar__title"># of Notes</div>
                 <div className='metric-bar__container'>
-                    <span className="metric-bar__value">240</span>
+                    <span className="metric-bar__value">
+                        {
+                            parseFloat(metrics.num_notes) < 0 ? metrics.num_notes : "+" + metrics.num_notes 
+                        }
+
+                    </span>
                     <span className="metric-bar__value-change">
-                        -11.4 <ArrowDownwardIcon className="metric-bar__icon negative" />
+                        {
+                            parseFloat(metrics.notes_delta) < 0 ?
+                                <ArrowDownwardIcon className='metric-bar__icon negative' /> :
+                                <ArrowUpwardIcon className='metric-bar__icon positive' />
+                        }
+
+                        {
+                            parseFloat(metrics.notes_delta) < 0 ?  metrics.notes_delta : "+" +  metrics.notes_delta 
+                        }
+
                     </span>
 
                 </div>
@@ -19,21 +43,48 @@ function Metricbar() {
             <div className="metric">
                 <div className="metric-bar__title"># of Contacts</div>
                 <div className='metric-bar__container'>
-                    <span className="metric-bar__value num_of_contacts">20</span>
+                <span className="metric-bar__value">
+                        {
+                            parseFloat(metrics.num_customers) < 0 ?  metrics.num_customers : "+" + metrics.num_customers 
+                        }
+
+                    </span>
                     <span className="metric-bar__value-change">
-                        +1 <ArrowUpwardIcon className="metric-bar__icon positive" />
+                        {
+                            parseFloat(metrics.customers_delta) < 0 ?
+                                <ArrowDownwardIcon className='metric-bar__icon negative' /> :
+                                <ArrowUpwardIcon className='metric-bar__icon positive' />
+                        }
+
+                        {
+                            parseFloat(metrics.customer_delta) < 0 ?  metrics.customer_delta : "+" + metrics.customer_delta 
+                        }
+
                     </span>
 
                 </div>
             </div>
             <div className="metric">
-                <div className="metric-bar__title">Avg Clients Added (per day)</div>
+                <div className="metric-bar__title">Avg Clients Added YTD</div>
                 <div className='metric-bar__container'>
-                    <span className="metric-bar__value avg_add_customers">2.55</span>
-                    <span className="metric-bar__value-change">
-                        -2 <ArrowDownwardIcon className="metric-bar__icon negative" />
-                    </span>
+                <span className="metric-bar__value">
+                        {
+                            parseFloat(metrics.num_customers) < 0 ?metrics.num_customers : "+" + metrics.num_customers 
+                        }
 
+                    </span>
+                    <span className="metric-bar__value-change">
+                        {
+                            parseFloat(metrics.customers_delta) < 0 ?
+                                <ArrowDownwardIcon className='metric-bar__icon negative' /> :
+                                <ArrowUpwardIcon className='metric-bar__icon positive' />
+                        }
+
+                        {
+                            parseFloat(metrics.customer_delta) < 0 ? metrics.customer_delta : "+" + metrics.customer_delta 
+                        }
+
+                    </span>
                 </div>
             </div>
 
