f2fc8848cf9fbbd81f2d5575544afe64cdda710c
Raj <raj350@mail.fresnostate.edu>
2022-03-21 06:50:38 -0700

    got image to be uploaded in public folder in the frontend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,14 +6,12 @@ const path = require('path');
 
 
 let img_path = path.join(__dirname,"../../frontend/public/images");
-console.log(img_path);
 
 let storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, img_path)
   },
   filename: function (req, file, cb) {
-    console.log(req.body);
     cb(null, file.fieldname + '-' + req.body.username);
   }
 })
