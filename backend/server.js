8079892e34039ff35f06f1f51ac967044a31032e
Raji <raj350@mail.fresnostate.edu>
2022-03-21 16:48:22 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,26 @@
 require("dotenv").config(); // ALLOWS ENVIRONMENT VARIABLES TO BE SET ON PROCESS.ENV SHOULD BE AT TOP
 
-const express = require("express");
-const app     = express();
-const cors    = require("cors");
+const express      = require("express");
+const app          = express();
+const cors         = require("cors");
+const sessions     = require('express-session');
+const cookieParser = require("cookie-parser");
+
 
 // Cross Origin 
 app.use(cors());
 
 // Middleware
 app.use(express.json()); // parse json bodies in the request object
+
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false
+}));
+app.use(cookieParser());
 
 
 // Routes
