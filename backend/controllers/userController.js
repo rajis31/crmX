2128630a275f6f821a42e6bef5b8318b56e47f56
Raji <raj350@mail.fresnostate.edu>
2022-03-21 16:48:48 -0700

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,16 @@
 const Users = require("../models/Users");
-
+let session;
 
 exports.loginUser = async (req, res, next) => {
   try{
-      let user    = new Users();
-      let username = req.body.username;
-      let password = req.body.password;
+      let user      = new Users();
+      session       = req.session;
+      let username  = req.body.username;
+      let password  = req.body.password;
 
-      let [result,_] = await user.findUser(username);
-      return result?.data?.length > 0 ? res.status(200).json({"msg":"User Found", "found":true}) : 
+      let authorize = await user.authorize(username,password);
+
+      return result?.length > 0 ? res.status(200).json({"msg":"User Found", "found":true}) : 
                                         res.status(404).send("User not Found");
   } catch(err){
     console.log("Something went wrong with user query.");
