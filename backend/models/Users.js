fb0dfc8c3f46b454628b834a03f2cffad4f983cb
Raji <raj350@mail.fresnostate.edu>
2022-03-21 12:13:05 -0700

    Update Users.js

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -1,13 +1,13 @@
 const db = require("../config/db");
 const bcrypt = require("bcrypt");
 
-class Users{
+class Users {
 
-    constructor(){
+    constructor() {
         this.tablename = "users";
     }
 
-    create_table(){
+    create_table() {
         let sql = `
             create table if not exists ${this.tablename} (
                 id int primary key auto_increment, 
@@ -20,36 +20,51 @@ class Users{
 
         return db.execute(sql);
     }
-    
 
-    async insert_data(username, password, date_created, email){
+
+    async insert_data(username, password, date_created, email, img_path) {
         /**
          * Insert a new row into table
          */
 
-        this.username     = username;
-        this.password     = await bcrypt.hash(password,10); 
-        this.email        = email; 
-        this.date_created = date_created;
+        password = await bcrypt.hash(password, 10);
 
         let sql = `
-            INSERT INTO ${this.tablename} (username, password, date_created) VALUES 
-            ( '${this.username}', '${this.password}'  ,'${this.date_created}' );
+            INSERT INTO ${this.tablename} (username, password, date_created, img_path) 
+            VALUES 
+            ( '${username}', '${password}'  ,'${date_created}', '${img_path}' );
         `;
 
         return db.execute(sql);
     }
 
-    async findUser(username){
+    async findUser(username) {
         /**
          * Retrieve Current user
          */
 
-        let sql =  `
+        let sql = `
             SELECT username, password FROM ${this.tablename} 
             WHERE username = '${username}';
         `
         return db.execute(sql);
+    }
+
+    async updateUser(username, img_path, user_id) {
+        /**
+         * Update user
+         */
+        let user_found = await this.findUser(username);
+        console.log(user_found);
+
+        let sql = `
+         UPDATE ${this.tablename}  
+            SET username = '${username}',
+                img_path = '${img_path}'
+         WHERE id = '${user_id}';
+        `
+        return db.execute(sql);
+
     }
 
 }
