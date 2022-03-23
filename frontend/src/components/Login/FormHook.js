724fb14d4405231c16e4f74d775dbd1ebfca8b64
Raji <raj350@mail.fresnostate.edu>
2022-03-23 16:13:21 -0700

    Create FormHook.js

---
+++ frontend/src/components/Login/FormHook.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { getCookie, setCookie } from "../../Helpers/Helpers";
+
+
+const formHook = (inputValues, validate) => {
+    const [inputs, setInputs] = useState(inputValues);
+    const [errors, setErrors] = useState({});
+    const [success, setSuccess] = useState();
+
+    useEffect(() => {
+        console.log(success);
+    }, [success]);
+
+
+    const navigate = useNavigate();
+
+    const handleLogin = (e) => {
+        const errorsFound = validate(inputs);
+        setErrors(errorsFound);
+        const hasErrors = Object.keys(errorsFound).length > 0;
+
+
+
+        const handleLoginCookie = (name, value, days) => {
+            setCookie(name, value, days);
+        }
+
+        if (!hasErrors) {
+            axios.post("http://localhost:3000/user/login",
+                {
+                    username: inputs.username,
+                    password: inputs.password,
+                })
+                .then(response => {
+                    console.log(response);
+                    if (response.status === 200) {
+                        response.data?.found ? 
+                                handleLoginCookie("session_id", response.data?.session_id, 30) :
+                                 "";
+                        navigate("/");
+                    } else {
+                        setSuccess(false);
+                    }
+                })
+                .catch(error => { setSuccess(false)  });
+        }
+    }
+
+    const handleInputChange = (e) => {
+        setInputs(prevState => ({ ...prevState, [e.target.name]: e.target.value }));
+    }
+
+    return {
+        handleInputChange,
+        handleLogin,
+        errors,
+        success
+    }
+}
+
+export default formHook;
