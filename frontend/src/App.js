2fe922780937d386312706798a8fe5a1ed7b7465
Raji <raj350@mail.fresnostate.edu>
2022-03-28 16:52:58 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,12 @@
-import React, { useState, lazy, Suspense, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { lazy, Suspense } from 'react';
+const LoginPage = lazy(() => import('./components/Login/Login'));
+const RegisterPage = lazy(() => import('./components/Register/Register'));
+const ForgotPage = lazy(() => import('./components/Forgot/Forgot'));
+const DashPage = lazy(() => import('./Pages/Dashboard/Dashboard'));
+import routes from "./routes/Routes";
 
 import PublicRoute from './routes/PublicRoute';
-import ProtectedRoutes from './routes/ProtectedRoutes';
-
-const LoginPage = lazy(() => import('./components/Login/Login'));
-const Register = lazy(() => import('./components/Register/Register'));
-const ForgotPassword = lazy(() => import('./components/Forgot/Forgot'));
-
-import routes from './routes/Routes';
+import PrivateRoute from './routes/PrivateRoute';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 
@@ -18,22 +16,12 @@ export default function App() {
   return (
     <div>
       <div className='container'>
-        <BrowserRouter>
-          <Suspense fallback={<Box><CircularProgress /></Box>}>
-            <Routes>
-              <Route
-                path='/login'
-                element={<PublicRoute><LoginPage /></PublicRoute>}
-                exact
-              />
-              <Route
-                path='/register'
-                element={<PublicRoute><Register /></PublicRoute>}
-                exact
-              />
-            </Routes>
-          </Suspense>
-        </BrowserRouter>
+        <Suspense fallback={<Box><CircularProgress /></Box>}>
+          <PublicRoute path="/login"><LoginPage /></PublicRoute>
+          <PublicRoute path="/register"><RegisterPage /></PublicRoute>
+          <PublicRoute path="/forgot"><ForgotPage /></PublicRoute>
+          <PrivateRoute path="/dash" component={DashPage}  />
+        </Suspense>
       </div>
     </div>
   );
