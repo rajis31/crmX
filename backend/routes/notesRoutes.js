a4e0bf83c732f0ddf9a07e0e35a6e6a8ed833b04
Raji <raj350@mail.fresnostate.edu>
2022-03-29 10:31:50 -0700

    Update notesRoutes.js

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -3,10 +3,10 @@ const NotesControllers = require("../controllers/NotesController");
 const router  = express.Router();
 
 // Notes Routes  
-router.route("/")
+router.route("/:session_id")
       .get(NotesControllers.getAllNotes);
 
-router.route("/report")
+router.route("/report/:session_id")
       .get(NotesControllers.generateNotesCsv);
       
 router.route("/add")
