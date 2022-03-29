f8e4efe5318f1a81d1ef8dcf7d395ee11b1e6834
Raji <raj350@mail.fresnostate.edu>
2022-03-29 11:46:17 -0700

    Update statsController.js

--- backend/controllers/statsController.js
+++ backend/controllers/statsController.js
@@ -1,25 +1,31 @@
 const Notes     = require("../models/Notes");
 const Customers = require("../models/Customers");
+const Users     = require("../models//Users");
+
 
 
 exports.getMetricStats = async (req, res, next) => {
     try{
-        const notes     = new Notes();
-        const customers = new Customers();
-        const username  = "raji";
+        const notes               = new Notes();
+        const customers           = new Customers();
+        const user                = new Users();
+        let session_id            = req.params.session_id;
+        let [result,_]            = await user.identify_user(session_id);
+        let username              = result[0]?.username;
 
         let [num_notes, ]         = await notes.countNotes(username);
-        let [num_customers, ]     = await customers.countCustomrs(username);
+        let [num_customers, ]     = await customers.countCustomers(username);
         let [avg_customers_ytd, ] = await customers.avgCustomersYTD(username);
         let [customer_delta,]     = await customers.delta(username,1);
-        let [notes_delta,]        = await notes.delta(username,1);         
+        let [notes_delta,]        = await notes.delta(username,1);    
+        
 
         const stats = {
-            num_notes: num_notes[0]["num_notes"],
-            num_customers: num_customers[0]["num_customers"],
+            num_notes:         num_notes[0]["num_notes"],
+            num_customers:     num_customers[0]["num_customers"],
             avg_customers_ytd: Math.round(avg_customers_ytd[0]["days_between"]*100)/100,
-            customer_delta: customer_delta[0]["diff"],
-            notes_delta:notes_delta[0]["diff"]
+            customer_delta:    customer_delta[0]["diff"],
+            notes_delta:       notes_delta[0]["diff"]
         }
         res.status(200).json(stats);
 
@@ -31,9 +37,12 @@ exports.getMetricStats = async (req, res, next) => {
 
   exports.geTopCustomers = async (req, res, next) => {
     try{
-        const customers = new Customers();
-        const username  = "raji";
-        let [topCustomers, ]         = await customers.topX(username,5);
+        const customers      = new Customers();
+        const user           = new Users();
+        let session_id       = req.params.session_id;
+        let username         = user.identify_user(session_id);
+
+        let [topCustomers, ] = await customers.topX(username,5);
         res.status(200).json(topCustomers);
 
     } catch(err){
@@ -47,7 +56,10 @@ exports.getMetricStats = async (req, res, next) => {
   exports.geCumulativeCustomerTotal = async (req, res, next) => {
     try{
         const customers    = new Customers();
-        const username     = "raji";
+        const user       = new Users();
+        let session_id   = req.params.session_id;
+        let username     = user.identify_user(session_id);
+
         let [cumulative, ] = await customers.cumulative(username);
         res.status(200).json(cumulative);
 
