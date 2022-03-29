89fa2df1ee867eeef63b5eb50c98a34abd497bd0
Raji <raj350@mail.fresnostate.edu>
2022-03-29 11:46:02 -0700

    Update statsRoutes.js

--- backend/routes/statsRoutes.js
+++ backend/routes/statsRoutes.js
@@ -3,13 +3,13 @@ const statsController = require("../controllers/statsController");
 const router  = express.Router();
 
 // Stats Routes  
-router.route("/get_metric_stats")
+router.route("/get_metric_stats/:session_id")
       .get(statsController.getMetricStats);
 
-router.route("/get_top_customers")
+router.route("/get_top_customers/:session_id")
       .get(statsController.geTopCustomers);
 
-router.route("/get_cumulative_customer_total")
+router.route("/get_cumulative_customer_total/:session_id")
       .get(statsController.geCumulativeCustomerTotal);
 
 module.exports = router;
