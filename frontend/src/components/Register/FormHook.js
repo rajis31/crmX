50d2c3fdcc2fa6cacc5104d15b85baaf6c021c5c
Raji <raj350@mail.fresnostate.edu>
2022-03-30 10:50:37 -0700

    Update FormHook.js

--- frontend/src/components/Register/FormHook.js
+++ frontend/src/components/Register/FormHook.js
@@ -1,45 +1,45 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { useLocation } from "wouter";
 
 const formHook = (inputValues, validate) => {
-    const [inputs, setInputs] =  useState(inputValues);
-    const [errors, setErrors] =  useState({});
+    const [inputs, setInputs] = useState(inputValues);
+    const [errors, setErrors] = useState({});
     const [success, setSuccess] = useState(false);
     const [location, setLocation] = useLocation();
 
-    const handleLogin = (e) =>{
+    const handleLogin = (e) => {
         setLocation("/login");
     }
 
-    const handleRegistration = (e) => {
-        const errorsFound = validate(inputs);
+    const handleRegistration = async (e) => {
+        const errorsFound = await validate(inputs);
         setErrors(errorsFound);
         const hasErrors = Object.keys(errorsFound).length > 0;
 
         const timeout = (delay) => {
-            return new Promise( res => setTimeout(res, delay) );
+            return new Promise(res => setTimeout(res, delay));
         }
 
-        if(!hasErrors){
+        if (!hasErrors) {
             axios.post("http://localhost:3000/user/register",
-            {
-                username: inputs.username,
-                password: inputs.password,
-                email:    inputs.email
-            })
-            .then(async (response) =>  {
-                setSuccess(true);
-                await timeout(5000);
-                setLocation("/login");
-            })
-            .catch(error => setSuccess(false));
+                {
+                    username: inputs.username,
+                    password: inputs.password,
+                    email: inputs.email
+                })
+                .then(async (response) => {
+                    setSuccess(true);
+                    await timeout(5000);
+                    setLocation("/login");
+                })
+                .catch(error => setSuccess(false));
         }
     }
     const handleInputChange = (e) => {
-        setInputs(prevState => ({...prevState, [e.target.name]: e.target.value}));
+        setInputs(prevState => ({ ...prevState, [e.target.name]: e.target.value }));
     }
-    return{
+    return {
         handleRegistration,
         handleInputChange,
         handleLogin,
