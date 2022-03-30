cc43c3e8a3d1c4f1dd4ee8080b0126b287322408
Raji <raj350@mail.fresnostate.edu>
2022-03-30 10:49:03 -0700

    Update Validate.jsx

--- frontend/src/components/Register/Validate.jsx
+++ frontend/src/components/Register/Validate.jsx
@@ -1,23 +1,33 @@
-const validate = (inputs) =>{
+import axios from "axios";
+
+const validate = async (inputs) => {
     const errors = {};
     console.log(inputs);
 
-    if(inputs.email.length === 0 || inputs.email === null){
+    if (inputs.email.length === 0 || inputs.email === null) {
         errors.emailError = true;
     }
 
-    if(inputs.username.length === 0 || inputs.username.length === null){
+    if (inputs.username.length === 0 || inputs.username.length === null) {
         errors.usernameError = true;
     }
 
-    if(inputs.password.length === 0 || inputs.password === null){
+    if (inputs.password.length === 0 || inputs.password === null) {
         errors.passwordError = true;
     }
 
-    if(inputs.password !== inputs.confirm_password || 
-      ( inputs.password.length === 0 || inputs.confirm_password.length === 0))
-      {
+    if (inputs.password !== inputs.confirm_password ||
+        (inputs.password.length === 0 || inputs.confirm_password.length === 0)) {
         errors.confirmpasswordError = true;
+    }
+
+    // Check if username exists 
+    let usernameCheck = await axios.post("http://localhost:3000/user/check_username",
+        { username: inputs.username });
+
+    console.log(usernameCheck);
+    if (usernameCheck.data.found) {
+        errors.userNameCheckError = true;
     }
 
     return errors;
