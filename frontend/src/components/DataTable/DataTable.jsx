f7144e6364c1706596b07ae1a6722947a12d667f
Raji <raj350@mail.fresnostate.edu>
2022-03-31 09:05:43 -0700

    Update DataTable.jsx

--- frontend/src/components/DataTable/DataTable.jsx
+++ frontend/src/components/DataTable/DataTable.jsx
@@ -17,9 +17,9 @@ import { Typography } from '@mui/material';
 import { TextField } from '@mui/material';
 import TextareaAutosize from '@mui/material/TextareaAutosize';
 import axios from "axios";
-import { getCookie } from "../../Helpers/Helpers";
+import { getCookie, generateUrl } from "../../Helpers/Helpers";
 
-function DataTable(props) {
+function DataTable() {
   const [rows, setRows] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [showAddForm, setShowAddModal] = useState(false);
@@ -53,7 +53,7 @@ function DataTable(props) {
   useEffect(() => {
     async function fetchData() {
       let session_id = getCookie("session_id");
-      const request = await axios.get("http://localhost:3000/notes/"+session_id);
+      const request = await axios.get(generateUrl("notes/"+session_id));
       setRows(request.data);
     }
     fetchData();
@@ -61,7 +61,7 @@ function DataTable(props) {
 
   async function deleteRow(id) {
     let session_id = getCookie("session_id");
-    axios.post("http://localhost:3000/notes/delete", { id: id, session_id: session_id })
+    axios.post(generateUrl("notes/delete"), { id: id, session_id: session_id })
       .then(response => {
         console.log(response);
         let filteredData = rows.filter(row => row.id !== id);
@@ -74,7 +74,7 @@ function DataTable(props) {
 
   async function addRow() {
     let session_id = getCookie("session_id");
-    axios.post("http://localhost:3000/notes/add",
+    axios.post(generateUrl("notes/add"),
       {
         title: titleRef.current.value,
         body: bodyRef.current.value,
