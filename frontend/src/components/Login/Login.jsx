c0efccfec80ef6235069dd86151219ce78efd501
Raji <raj350@mail.fresnostate.edu>
2022-03-31 09:17:33 -0700

    Update Login.jsx

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,117 +1,57 @@
-import React, { useRef } from 'react'
-import "./Login.css";
-import { Button, TextField, Typography, Paper } from '@mui/material';
-import { useLocation } from 'wouter';
-import Alert from '@mui/material/Alert';
-import formHook from './FormHook';
-import validate from './Validate';
-import Slide from '@mui/material/Slide';
-import Box from '@mui/material/Box';
-
-export default function Login() {
-    const inputValues = { username: "", password: "" };
-    const { handleInputChange, handleLogin, errors, success } = formHook(inputValues, validate);
-    const [location, setLocation] = useLocation();
-    const containerRef = useRef();
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useLocation } from "wouter";
+import { setCookie, generateUrl } from "../../Helpers/Helpers";
 
 
-    const handleRegisterBtn = (e) => {
-        setLocation("/register");
+const formHook = (inputValues, validate) => {
+    const [inputs, setInputs] = useState(inputValues);
+    const [errors, setErrors] = useState({});
+    const [success, setSuccess] = useState();
+
+    const handleLogin = (e) => {
+        const errorsFound = validate(inputs);
+        setErrors(errorsFound);
+        const hasErrors = Object.keys(errorsFound).length > 0;
+
+        const handleLoginCookie = async (name, value, days) => {
+            setCookie(name, value, days);
+        }
+
+        if (!hasErrors) {
+            axios.post(generateUrl("user/login"),
+                {
+                    username: inputs.username,
+                    password: inputs.password,
+                })
+                .then(async (response) => {
+
+                    if (response.status === 200) {
+                        if(response.data?.found ){
+                            await handleLoginCookie("session_id", response.data?.session_id, 30);
+
+                            let protocal = window.location.protocol;
+                            let hostname = window.location.host;
+                            window.location.href=protocal+"//"+hostname+"/";
+                        }
+                    } else {
+                        setSuccess(false);
+                    }
+                })
+                .catch(error => { setSuccess(false)  });
+        }
     }
 
-    const handleForgotBtn = (e) => {
-        setLocation("/forgot");
+    const handleInputChange = (e) => {
+        setInputs(prevState => ({ ...prevState, [e.target.name]: e.target.value }));
     }
 
-    return (
-        <div className='login-form-container'>
-            <h1>login</h1>
-            <form>
-                <label htmlFor="username">username</label>
-                <TextField
-                    placeholder='Type your Username'
-                    type="text"
-                    name='username'
-                    onChange={handleInputChange}
-                />
-                {
-                    errors.usernameError ?
-                        <Alert
-                            severity="error"
-                            className='login-form__error-msg'
-                        >
-                            Please type your username in
-                        </Alert> :
-                        <></>
-                }
-                <label htmlFor="password">password</label>
-                <TextField
-                    placeholder='Type your Password'
-                    type="password"
-                    name='password'
-                    onChange={handleInputChange}
-                />
-                {
-                    errors.passwordError ?
-                        <Alert
-                            severity="error"
-                            className='login-form__error-msg'
-                        >
-                            Please type your password in
-                        </Alert> :
-                        <></>
-                }
-
-                {
-                    success === false ?
-                        <Alert
-                            severity="error"
-                            className='login-form__error-msg'
-                        >
-                            Could not login. Please try again
-                        </Alert> :
-                        <></>
-                }
-
-            </form>
-            <p className="forgot" onClick={() => { handleForgotBtn() }}>forgot password?</p>
-            <div className='login-form__btn-group'>
-                <Button
-                    variant="contained"
-                    color="secondary"
-                    onClick={handleLogin}
-                >
-                    Login Here
-                </Button>
-
-                <Button
-                    variant="contained"
-                    color="success"
-                    onClick={handleRegisterBtn}
-                >
-                    Register Here
-                </Button>
-            </div>
-
-          
-
-            <div className='login_tag'>
-                <a 
-                    className='login_tag_link' 
-                    href='https://www.dev-top.com'
-                    >
-                        Created by Raj Solanki 
-                        <br />
-                        (https//www.dev-top.com)
-                </a>
-                <br />
-                <p
-                    style={{color: "white"}}
-                >
-                    This site uses cookies
-                </p>
-            </div>
-        </div>
-
-    )
+    return {
+        handleInputChange,
+        handleLogin,
+        errors,
+        success
+    }
 }
+
+export default formHook;
