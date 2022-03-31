25247375eef426f9bb50c3babdb576b6c575e651
Raji <raj350@mail.fresnostate.edu>
2022-03-31 09:14:41 -0700

    Update Reports.jsx

--- frontend/src/Pages/Reports/Reports.jsx
+++ frontend/src/Pages/Reports/Reports.jsx
@@ -2,65 +2,65 @@ import React from 'react';
 import axios from "axios";
 import { Button, Typography } from '@mui/material';
 import Sidebar from '../../components/Sidebar/Sidebar';
-import {getCookie} from"../../Helpers/Helpers"
+import { getCookie, generateUrl } from "../../Helpers/Helpers"
 
 function Reports() {
-  const NotesReport = ()=>{
+  const NotesReport = () => {
     axios({
-        url: 'http://localhost:3000/notes/report/'+getCookie("session_id"),
-        method: 'GET',
-        responseType: 'blob'
-        
+      url:  generateUrl("notes/report/"+ getCookie("session_id")) ,
+      method: 'GET',
+      responseType: 'blob'
+
     }).then((response) => {
-        const url = window.URL.createObjectURL(new Blob([response.data]));
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', 'Notes.csv'); 
-        document.body.appendChild(link);
-        link.click();
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'Notes.csv');
+      document.body.appendChild(link);
+      link.click();
     });
   }
 
-  const CustomersReport = ()=>{
+  const CustomersReport = () => {
     axios({
-        url: 'http://localhost:3000/customers/report/'+getCookie("session_id"),
-        method: 'GET',
-        responseType: 'blob', 
+      url:  generateUrl("customers/report/"+ getCookie("session_id")) ,
+      method: 'GET',
+      responseType: 'blob',
 
     }).then((response) => {
-        const url = window.URL.createObjectURL(new Blob([response.data]));
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', 'Customers.csv'); 
-        document.body.appendChild(link);
-        link.click();
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'Customers.csv');
+      document.body.appendChild(link);
+      link.click();
     });
   }
 
   return (
     <div>
-        <Sidebar />
-        <Typography 
-          variant='h4'
-          style={{marginLeft: "80px"}}
-        >
-          Generate Reports
-        </Typography>
-        <Button 
-            onClick={NotesReport}
-            variant="contained"
-            style={{marginLeft: "80px", marginTop: "20px"}}
-        >
-          Get all Notes
-        </Button>
-        <br />
-        <Button 
-            onClick={CustomersReport}
-            variant="contained"
-            style={{marginLeft: "80px",  marginTop: "20px"}}
-        >
-          Get all Customers
-        </Button>
+      <Sidebar />
+      <Typography
+        variant='h4'
+        style={{ marginLeft: "80px" }}
+      >
+        Generate Reports
+      </Typography>
+      <Button
+        onClick={NotesReport}
+        variant="contained"
+        style={{ marginLeft: "80px", marginTop: "20px" }}
+      >
+        Get all Notes
+      </Button>
+      <br />
+      <Button
+        onClick={CustomersReport}
+        variant="contained"
+        style={{ marginLeft: "80px", marginTop: "20px" }}
+      >
+        Get all Customers
+      </Button>
     </div>
   )
 }
